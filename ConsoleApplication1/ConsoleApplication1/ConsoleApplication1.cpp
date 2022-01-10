328e511a395c2c0572b7ea1ddf9b7cb2bcfdd2d2
Daisy <doanthicuc.daisy94@gmail.com>
2022-01-10 11:20:03 -0800

    Creating solution for project

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
+
